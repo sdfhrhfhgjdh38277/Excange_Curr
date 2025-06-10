a91eec769b085bebf0bb0c8864e72ebfc7a56c9e
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2025-06-10 12:11:19 +0300

    Update main.py
    Edited:
    1. Supported currencies to better UX
    2. User can write needed currency to this field
    3. App supporting all currencies

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import streamlit as st
 from currencies.parse import Currency
 
+st.set_page_config(page_title="Exchange Calculator", page_icon="💸")
 
-st.set_page_config(page_title='Excange Calculator', page_icon='💸')
+st.markdown(
+    "### Want know how much you need give to the bank for needed amount of currency?"
+)
 
-st.markdown("**Want know how much you need give to the bank for needed amount of currency?** Calculate this on my Currency-Calculator! Just select amount, two currencies and thats all!")
+st.markdown(
+    "Calculate this for free! Just select amount, two currencies and that's all."
+)
 
+currency = Currency()
+supported = currency.supported_currencies
 
-# Choise the currency
-from_currency = st.selectbox("From currency", ['USD', 'UAH', 'RUB'])
+from_currency = st.selectbox("From currency", supported)
 
-to_currency_options = [curr for curr in ['USD', 'UAH', 'RUB'] if curr != from_currency]
-
+to_currency_options = [curr for curr in supported if curr != from_currency]
 to_currency = st.selectbox("To currency", to_currency_options)
 
-amount = st.number_input(
-    label="Enter amount", 
-    step=0.01, 
-    max_value=float(1_000_000)
-)
+amount = st.number_input(label="Enter amount", step=0.01, max_value=float(1_000_000))
+
 if st.button("Check amount"):
-    currency = Currency()
     clean_amount = abs(amount)
     result = currency.convert(
-        to_curr=to_currency, 
-        from_curr=from_currency,
-        amount=clean_amount
+        from_curr=from_currency, to_curr=to_currency, amount=clean_amount
     )
-    print("DEBUG amount: ", clean_amount)
     if result is not None:
-        st.markdown(f"### 💰 {clean_amount} {from_currency} = **{result:.2f} {to_currency}**")
+        st.markdown(
+            f"### 💰 {clean_amount} {from_currency} = **{result:.2f} {to_currency}**"
+        )
     else:
         st.error("Conversion failed.")
