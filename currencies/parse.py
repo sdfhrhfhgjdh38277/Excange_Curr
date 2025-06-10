bffb14a67dd6f5e085c2a98ec3f0c69891e47563
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2025-06-10 12:15:16 +0300

    Update parse.py
    Global update scraping, added more currencies from API-giver

--- currencies/parse.py
+++ currencies/parse.py
@@ -3,12 +3,28 @@ import requests
 
 class Currency:
     def __init__(self):
-        pass
+        self.api_key = "API-KEY"
+        self.supported_currencies = self.get_supported_currencies()
+
+    def get_supported_currencies(self):
+        url = f"https://v6.exchangerate-api.com/v6/{self.api_key}/latest/USD"
+        response = requests.get(url).json()
+        if response["result"] == "success":
+            return list(response["conversion_rates"].keys())
+        else:
+            print("Failed to get supported currencies:", response)
+            return []
 
     def convert(self, from_curr, to_curr, amount):
-        url = f"https://v6.exchangerate-api.com/v6/183746f5f8587ca8e2703602/pair/{from_curr}/{to_curr}/{amount}"
-        response = requests.get(url).json()
+        if (
+            from_curr not in self.supported_currencies
+            or to_curr not in self.supported_currencies
+        ):
+            print("One of the currencies is not supported.")
+            return None
 
+        url = f"https://v6.exchangerate-api.com/v6/{self.api_key}/pair/{from_curr}/{to_curr}/{amount}"
+        response = requests.get(url).json()
         if response["result"] == "success":
             return response["conversion_result"]
         else:
